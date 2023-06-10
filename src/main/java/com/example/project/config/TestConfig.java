aa4425992db5df2c71db514cfd7946772857102d
werner <carlos.werner@pathfind.com.br>
2023-06-10 17:17:29 -0300

    add order status enum

--- src/main/java/com/example/project/config/TestConfig.java
+++ src/main/java/com/example/project/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.example.project.config;
 
 import com.example.project.entities.User;
 import com.example.project.entities.Order;
+import com.example.project.entities.enums.OrderStatus;
 import com.example.project.repositories.OrderRepository;
 import com.example.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,9 +37,9 @@ public class TestConfig implements CommandLineRunner {
         // salvando todos os usuários no banco de dados
         userRepository.saveAll(Arrays.asList(u1, u2));
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.SHIPPED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED, u1);
 
         // salvando todos os pedidos
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/example/project/entities/Order.java
+++ src/main/java/com/example/project/entities/Order.java
@@ -1,5 +1,6 @@
 package com.example.project.entities;
 
+import com.example.project.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -35,9 +38,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -56,6 +60,14 @@ public class Order implements Serializable {
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public OrderStatus getOrderStatus() { return OrderStatus.valueOf(orderStatus); }
 
     @Override
     public boolean equals(Object o) {
